5a9a6522c9728a1eb4db5ada0652a0ea3111587d
leopon55 <re.o.5123re.o@gmail.com>
2020-12-06 14:22:50 +0900

    仮実装done

--- lib/poll.rb
+++ lib/poll.rb
@@ -1,11 +1,20 @@
-class Poll
-    def initialize(title, candidates)
-    end
+# class Poll    
+#     def initialize(title, candidates)
+#     end
 
-    def title
-        "Awesome Poll"
+#     def title
+#         "Awesome Poll"
+#     end
+#     def candidates
+#         ['Alice', 'Bob']
+#     end
+# end
+
+class Poll
+    attr_reader :title, :candidates
+  
+    def initialize(title, candidates)
+      @title = title
+      @candidates = candidates
     end
-    def candidates
-        ['Alice', 'Bob']
-    end
-end
+  end
